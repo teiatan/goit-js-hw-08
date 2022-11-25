e6455a5db6d62795d613467ae1c556809eeb45c8
teiatan <92535482+teiatan@users.noreply.github.com>
2022-11-26 01:14:30 +0200

    simplelightbox is added without styles

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(`.gallery`);
+
+const makeGalleryCard = ({original, preview, description}) => `
+    <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+`;
+const galleryItemsEl = galleryItems.map(makeGalleryCard);
+galleryContainer.insertAdjacentHTML(`afterbegin`, galleryItemsEl.join(""));
+new SimpleLightbox('.gallery a'/* , {
+    captions: true,
+    captionSelector: 'img',
+    captionPosition: 'bottom',
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionDelay: 250,
+    } */);
