3bbf669540c1881f750fdf0bd0bac9ded4f1d02c
teiatan <92535482+teiatan@users.noreply.github.com>
2022-12-01 15:43:33 +0200

    video player saves time

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+ 
+player.on('timeupdate', throttle(saveTime, 1000));
+
+function saveTime() {
+    player.getCurrentTime().then(function (seconds) {
+        localStorage.setItem('videoplayer-current-time', seconds);
+    });
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
